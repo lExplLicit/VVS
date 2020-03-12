299b3a52ba826eac93cf8bc090b99110ed2ae7a1
Robert Eckermann <info@roberteckermann.eu>
2020-03-12 16:25:10 +0100

    typo fix

--- login.php
+++ login.php
@@ -164,7 +164,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <br>
                     </form>
                     <br>
-                    <p style="text-align: center">VVS Verison 1.0.0</p>
+                    <p style="text-align: center">VVS Version 1.0.0</p>
                 </div>
             </div>
 
